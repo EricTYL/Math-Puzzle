66f648c369ed68f3c6b6bf1abd2d213925221f61
RickyTYL <rickytyl@icloud.com>
2017-05-05 10:19:34 +0800

    q4 done

---
+++ q4.rb
@@ -0,0 +1,20 @@
+def cut(m,n,bars)
+  if bars == n
+    0
+  elsif bars > n
+    puts "Error appear, bars never greater than n."
+  else
+    if bars <= m
+      1 + cut(m,n,bars * 2)
+    else
+      if bars + m < n
+	1 + cut(m,n,bars + m)
+      else
+	1 + cut(m,n,n) # set bars = n in order to end recursion
+      end
+    end
+  end
+end
+
+puts cut(3,20,1)
+puts cut(5,100,1)
