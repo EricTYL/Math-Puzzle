e6620f5d9bb476e1bfef5d61a01c63f8f52908af
RickyTYL <rickytyl@icloud.com>
2017-05-18 08:59:19 +0800

    add q3.rb comment

--- q3.rb
+++ q3.rb
@@ -1,9 +1,16 @@
 (1..100).each do |i|
+
+  # 預設編號 1 - 100 卡牌蓋牌 
   flag = false
   (1..i).each do |j|
     if i % j == 0
+      
+      # 發現因數翻牌一次
       flag = !flag
     end
   end
+
+  # 擁有奇數個因數的卡牌最後會翻面，
+  # 比如卡牌 4 有 1,2,4 三個因數，結果正面朝上
   puts i if flag
 end
